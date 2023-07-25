bef86399db41b46bea6c180dff9cbd2af54a111b
Zihao Xia <12144094+doctorzippy@users.noreply.github.com>
2023-07-25 00:19:14 -0700

    Add solution for 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+const storage = [];
+for (let i = input.length; i>=0; i--) {
+    storage.push(input.charAt(i));
+}
+return storage.join('');
 };
 
 // Do not edit below this line
