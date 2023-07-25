c3c9b5f5f6df95543a8320e91c175d4b9e30bf47
Zihao Xia <12144094+doctorzippy@users.noreply.github.com>
2023-07-25 00:09:49 -0700

    Add solution for 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(input,n) {
+    if (n<0) {
+        return "ERROR"
+    }
+    else {
+    let start = "";    
+    for (let i = 0;i<n;i++) {
+        start += input;
+}
+return start;
+}
+}
 // Do not edit below this line
 module.exports = repeatString;
