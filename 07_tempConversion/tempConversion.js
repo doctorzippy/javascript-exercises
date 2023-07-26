78583c1807085bb7577b9fa794e2fc740fd83a39
Zihao Xia <12144094+doctorzippy@users.noreply.github.com>
2023-07-25 23:19:41 -0700

    Add solution for 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ftemp) {
+  let conversion = (ftemp-32)*5/9;
+  return Math.round(conversion*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ctemp) {
+  let conversion = ctemp*9/5+32;
+  return Math.round(conversion*10)/10;
 };
 
 // Do not edit below this line
