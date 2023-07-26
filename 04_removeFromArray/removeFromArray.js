fac456a738e6785537778aa8c0f64364be65345a
Zihao Xia <12144094+doctorzippy@users.noreply.github.com>
2023-07-25 17:23:43 -0700

    Add solution for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(input) {
+  const args = Array.prototype.slice.call(arguments, 1);
+  const output = input.filter(element => !args.includes(element));  
+    return output;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
